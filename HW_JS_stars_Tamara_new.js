49fef100bc84cd5a11e397a124e32c21fe72b9d5
Tomullya <61884339+Tomullya@users.noreply.github.com>
2022-06-22 19:10:39 +0300

    Add files via upload

---
+++ HW_JS_stars_Tamara_new.js
@@ -0,0 +1,20 @@
+
+let age_2=18, 
+age_3=60;
+let age = prompt("Please, enter your age ", '');
+const checkage = function (){
+if (+age<age_2){
+alert("You don’t have access cause your age is " + +age +['.'] + " It's less then " +  age_2+['.']);
+}
+else if (+age>=  age_2, +age <  age_3) 
+{alert("Welcome !");
+}
+else if(+age > age_3)
+{alert("Keep calm and look Culture channel");
+}
+
+else {alert("Error!Technical work");
+}
+};
+checkage();
+alert("Thanks for visiting us!");
